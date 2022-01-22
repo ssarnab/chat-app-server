9c11a038cb6dfcd77cd47ffcc408e8c0c2d309f6
Shakib Sarar Arnab <arnab0000007@gmail.com>
2022-01-22 22:50:18 +0600

    socket code added

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
+//
+const socketio = require('socket.io');
+const http = require('http');
+//
 const cors = require('cors');
 const helmet = require("helmet");
 const morgan = require("morgan");
@@ -28,12 +31,61 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("common"));
 
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/conversations", conversationRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(port, () => {
-  console.log("Backend server is running!");
+// app.listen(port, () => {
+//   console.log("Backend server is running!");
+// });
+////////
+
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
+
+io.on('connection', (socket) => {
+   //when ceonnect
+   console.log("a user connected.");
+
+   //take userId and socketId from user
+   socket.on("addUser", (userId) => {
+     addUser(userId, socket.id);
+     io.emit("getUsers", users);
+   });
+ 
+   //send and get message
+   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+     const user = getUser(receiverId);
+     io.to(user.socketId).emit("getMessage", {
+       senderId,
+       text,
+     });
+   });
+ 
+   //when disconnect
+   socket.on("disconnect", () => {
+     console.log("a user disconnected!");
+     removeUser(socket.id);
+     io.emit("getUsers", users);
+   });
 });
+
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId ===  userId);
+};
+
